7137391b439c4a46f23f7cb68370519b3c41c327
SpartanII <haloreach3@163.com>
2016-01-18 00:33:58 -0500

    fullpage.js
    use fullpage.js to get full page switch

--- web/js/resume.js
+++ web/js/resume.js
@@ -1,43 +1,47 @@
 $(function(){
 	
-	//general "just a little different"
-		$('.general span').animate({opacity: 1},500,function(){
-			$('.general p').animate({opacity: 1},500,function(){
+		$('#fullpage').fullpage({
+			verticalCentered:false,
+			anchors: ['NavScreen','GeneralScreen','ExperienceScreen','ProjectScreen','EducationScreen','SkillScreen','HobbyScreen','FooterScreen'],
+			
+			afterLoad: function(anchorLink, index){
+				var loadedSection = $(this);
+	
+				//using index
+				if(index == 2){
+					$('.general p').stop().animate({opacity: 1},500,function(){
 				$('.general img').animate({opacity: 1, top: 50+'%'},800,function(){
 					$('.general h1').animate({opacity: 1},1000);});
-		});});
+		});		
+				}
+				if(index == 3){
+					$('.experience_text').animate({opacity: 1, top: 20+'%', right: '10%'},500);
+					$('.experience_bar').animate({opacity: 1, top: 77+'%', left: '10%'},500);
+				}
+				if(index == 4){
+					$('.projects_text').stop().animate({opacity: 1, top: '6%'}, 400);
+					$('.projects_1 img').stop().animate({width:'150px', marginTop:'100px'},400);
+					$('.projects_2 img').stop().animate({width:'100px', marginTop:'80px'},400);
+					$('.projects_3 img').stop().animate({width:'150px', marginTop:'120px'},400);
+					$('.projects_4 img').stop().animate({width:'150px', marginTop:'100px'},400);
+				}
+				if(index == 5){
+					$('.education_text').stop().animate({left: 0},800);
+					$('.schools').stop().animate({left: 0},800);
+				}
+				if(index == 6){
+					$('.skills_text').animate({top: 3+'%'}, 800);
+					$('.skills_ul1').stop().animate({marginLeft: '35%'},800);
+					$('.skills_ul2').stop().animate({marginRight: '35%'},800);
+				}
+				if(index == 7)
+					$('.hobbies_list').stop().animate({height: 600},800);
+			}
+		});
 		
-	//check the distance from the top
-	var distance;
-	$(document).scroll(function(){
-			distance = $(this).scrollTop();	//这个数字决定下拉多少
-			console.log(distance);
+	//general "just a little different"
+		$('.general span').animate({opacity: 1},500);	
 			
-			/*if(distance>0 && distance<847)
-				$('body,html').animate({'scrollTop':847},500);
-			else if(distance>847 && distance<1694)
-				$('body,html').animate({'scrollTop':1694},500);
-			else if(distance>1694 && distance<2500)
-				$('body,html').animate({'scrollTop':2500},500);*/
-				
-			if(distance>400)
-				$('.experience_text').animate({opacity: 1, top: 20+'%'},1000);
-			if (distance>830)
-				$('.experience_bar').animate({opacity: 1, top: 77+'%'},1000);
-			if(distance>1600)
-			{
-				$('.projects_1 img').stop().animate({width:'150px', marginTop:'100px'},400);
-				$('.projects_2 img').stop().animate({width:'100px', marginTop:'80px'},400);
-				$('.projects_3 img').stop().animate({width:'150px', marginTop:'120px'},400);
-				$('.projects_4 img').stop().animate({width:'150px', marginTop:'100px'},400);
-			}
-			if(distance>2400)
-				$('.schools').stop().animate({left: 0},800);
-			if(distance>3000){
-				$('.skills_ul1').stop().animate({marginLeft: '35%'},800);
-				$('.skills_ul2').stop().animate({marginRight: '35%'},800);
-			}
-		}); 		
 	
 	var index = 0;
 	var gl = new Array("Hello, Welcome to my Website!","Hi, there!", "How are you doing today?","What can I do for you?","Please go through my profile below, you will find what you want!","Please don't click me", "Don't","I warn you, do not click me!","You think just because I am smiling, I will not piss off?", "If you do it again, I refuse to talk to you", "I am not going to talk to you anymore","...not any more");
